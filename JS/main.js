e6d2bb2b6e7849b784e2bfd73b565e97d74d87b7
Manuel Navarro <manuel.alejandro.navarro@gmail.com>
2018-09-14 23:29:04 -0500

    ganar listo

--- JS/main.js
+++ JS/main.js
@@ -25,7 +25,7 @@ var score = 0;
 //Helpers
 
 function start(){
-    fondo.music.play();
+    // fondo.music.play();
     interval = setInterval(update, 1000 / 60);
 }
 
@@ -33,6 +33,7 @@ function update() {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     control();  
+    // fondoCae = pararFondo();
     fondoCae = pararFondo();
     fondoMovimiento();
     fondo.draw();
@@ -51,36 +52,85 @@ function restart(){
     start();
 }
 
-function gameOver() {  
-    console.log("valiste");
-    fondo.gameOver();
+/* function gameOver() {  
     clearInterval(interval);
-    // ctx.font = "30px Avenir"
-    // ctx.fillText("Oh no, se derritieron todas las chispas", 1250, 200)
-    // ctx.fillText("Presiona Enter para intentarlo de nuevo", 1250, 240);   
+    if (pararFondo=true);
+    perder();
+    // $(".win").css("opacity", "100");
+    // $(".win h2").text('¡Oh no! Crispy se cayó y hay pedazos de galleta por todo el piso. <br> Presiona R para intentarlo de nuevo.');
+    // // $(".win p").text(`Score: ${this.points}`);
+    // $("#next-level").css("display", "none");
+    // $("canvas").css("display", "none");
+    // $("body").text('¡Oh no! Crispy se cayó y hay pedazos de galleta por todo el piso. Presiona R para intentarlo de nuevo.'); 
+    
+    // fondo.music.pause();
+    ctx.font = "30px Avenir"
+    ctx.fillText("Oh no, se derritieron todas las chispas", 1250, 200)
+    ctx.fillText("Presiona Enter para intentarlo de nuevo", 1250, 240);   
     // $("canvas").css("display", "none");
     // $("body").text('You lost! try again'); 
+} */
+
+
+function pararFondo(){
+    // console.log(cookie.y )
+    if(fondo.y > -100 || 
+        cookie.y + 2 > 494) {
+            if(-2183 < fondo.y && cookie.y + 2 > 494) {
+                gameOver();
+            }
+            // if (fondo.y)
+            return  false;
+    } 
+        return true;
 }
 
+
+function gameOver(){
+    if(pararFondo === false);
+    clearInterval(interval);
+    console.log("muerte")
+    ctx.font = "30px Avenir"
+    ctx.fillText("Oh no, hay pedazos de Crispy por todo el piso", 0, 200)
+    ctx.fillText("Presiona Enter para intentarlo de nuevo", 250, 240);
+}
 
 function ganar (){
+    console.log("ganas")
     clearInterval(interval);
-    $("canvas").css("display", "none");
-    $("body").text('You win! Your score is: ' + score );
+    
+    
+    //texto
+    //primera línea
+    ctx.font='bold 30px Verdana';
+    ctx.fillStyle="#3c2b11";
+    ctx.textAlign = "center";
+    ctx.fillText("Crispy es más chocolatoso gracias a ti.", canvas.width/2, 200);
+    
+    //segunda linea
+    ctx.font='25px Verdana';
+    ctx.fillStyle="#ec730f";
+    ctx.textAlign = "center";
+    ctx.fillText("Lograste conseguir " + score + " chispas.", canvas.width/2,240);
+    
+    //tercer linea
+    ctx.font='18px Verdana';
+    ctx.fillStyle="#3c2b11";
+    ctx.textAlign = "center";
+    ctx.fillText("Presiona R para intentarlo de nuevo.", canvas.width/2, 290);
+    
+    //fondo transparente
+    ctx.beginPath ();
+    ctx.rect(0,0,canvas.width,canvas.height);
+    ctx.globalAlpha =0.3;  
+    ctx.fillStyle = 'white';
+    ctx.fill();
 }
-
-/*  if (timeleft() <= 0)
-console.log("timer jala")
-return;
-ctx.font = "30px Avenir"
-ctx.fillText("Oh no, se derritieron todas las chispas", 250, 200)
-ctx.fillText("Presiona Enter para intentarlo de nuevo", 250, 240); */
 
 
 function tiempoOver(){
     if (timeleft ===0)
-    ganar();
-    
+    ganar(); 
 }
 
 function cookieLeft (){
@@ -121,19 +171,6 @@ function salto (){
     }
     cookie.jumping = false;
 }
-
-function pararFondo(){
-    // console.log(cookie.y )
-    if(fondo.y > -100 || 
-        cookie.y + 2 > 494) {
-            if(-2183 < fondo.y && cookie.y + 2 > 494) {
-                gameOver();
-            }
-            // if (fondo.y)
-            return  false;
-        } 
-        return true;
-    }
     
     
     function drawPlatos(){
@@ -192,9 +229,9 @@ function pararFondo(){
     })
     
     //Checar este código
-    addEventListener("keyup", function (e){
+    addEventListener("keydown", function (e){
         // console.log(e.keyCode)
-        if (e.keyCode === 13) {
+        if (e.keyCode === 82) {
             restart();
         }
     })
